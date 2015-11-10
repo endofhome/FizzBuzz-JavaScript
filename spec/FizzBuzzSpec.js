7331493d4fd9f1f08cb777b628c91f9dabc6e43f
Tom Barnes <ruralmyth@gmail.com>
2015-11-10 12:00:35 +0000

    writing first test (unfinished)

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,12 @@
+describe("FizzBuzz", function() {
+  
+	it("should return Fizz if number is divisible by 3", function() {
+      expect(FizzBuzz(3)).toEqual('Fizz');
+	});
+});
+
+
+
+// it 'returns "fizz" when passed 3' do
+//   expect(fizzbuzz(3)).to eq 'fizz'
+// end
