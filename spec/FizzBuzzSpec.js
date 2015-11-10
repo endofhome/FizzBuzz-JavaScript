04f5c7987133cd7e211e3e33121135c2ab79f921
Robert Youngman <rob_youngman@yahoo.com>
2015-11-10 15:06:05 +0000

    fizzbuzz tests pass

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,12 +1,18 @@
 describe("FizzBuzz", function() {
-  
+
 	it("should return Fizz if number is divisible by 3", function() {
       expect(FizzBuzz(3)).toEqual('Fizz');
 	});
+
+  it("should return Buzz if number is divisible by 5", function() {
+      expect(FizzBuzz(5)).toEqual('Buzz');
+  });
+
+  it("should return FizzBuzz if number is divisible by 3 and 5", function() {
+    expect(FizzBuzz(15)).toEqual('FizzBuzz');
+  });
+
+  it("should return number if not divisible by 3 or 5", function() {
+    expect(FizzBuzz(4)).toEqual(4);
+  });
 });
-
-
-
-// it 'returns "fizz" when passed 3' do
-//   expect(fizzbuzz(3)).to eq 'fizz'
-// end
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,3 +1,15 @@
 function FizzBuzz(number) {
-	return 'Fizz';
+  if ((number%3 === 0) && (number%5 === 0)) {
+    return 'FizzBuzz';
+  }
+
+  if (number%3 === 0) {
+    return 'Fizz';
+  }
+
+  if (number%5 === 0) {
+    return 'Buzz';
+  }
+
+  return number
 }
